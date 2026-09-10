d761847ea323fcd573a99e8c0be7052fe447d8d7
Saad Patel <psaad2988@gmail.com>
2026-09-10 13:22:57 +0530

    Time: 39 ms (30.85%), Space: 12.6 MB (7.87%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        res = []
+        candidates.sort()
+
+        def solve(start, temp, target):
+            if target == 0:
+                res.append(temp[:])
+                return
+            if target < 0 or start >= len(candidates):
+                return
+
+            for i in range(start, len(candidates)):
+                # skip duplicates at same recursion level
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+
+                temp.append(candidates[i])
+                solve(i + 1, temp, target - candidates[i])
+                temp.pop()
+
+        solve(0, [], target)
+        return res
+
+
+   
+        
