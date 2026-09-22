d8a4a5eb3b31645cf7700a214ecaa5e8b85c94c0
Saad Patel <psaad2988@gmail.com>
2026-09-22 22:07:03 +0530

    Time: 23 ms (18.94%), Space: 26.5 MB (67.21%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
@@ -1,6 +1,3 @@
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
-        mini = min(nums)
-        maxi = max(nums)
-
-        return (maxi-mini)*k
+        return (max(nums)-min(nums))*k
