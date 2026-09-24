05e72c2ace761718e676238750cdb5760a7f2ec5
Saad Patel <psaad2988@gmail.com>
2026-09-24 10:48:09 +0530

    Time: 3 ms (72.48%), Space: 19.3 MB (55.83%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        l = 1
+        r = x
+        while l<=r:
+            mid = (l+r)//2
+            if mid*mid > x:
+                r = mid-1
+            else:
+                ans = mid
+                l = mid+1
+
+        return ans        
+
